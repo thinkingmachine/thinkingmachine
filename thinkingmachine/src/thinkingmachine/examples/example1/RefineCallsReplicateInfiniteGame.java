3b2a45e74d9f978822a528f59ca890712a04952e
Martin <Martin@WIN-G6RNOCGATRM>
2015-06-20 12:22:24 +0100

    tidy up code

--- thinkingmachine/src/thinkingmachine/examples/example1/RefineCallsReplicateInfiniteGame.java
+++ thinkingmachine/src/thinkingmachine/examples/example1/RefineCallsReplicateInfiniteGame.java
@@ -1,69 +1,66 @@
 package thinkingmachine.examples.example1;
 
 public class RefineCallsReplicateInfiniteGame {
-	
-	
+
 	/*
 	 * 
 	 * Refine the program based on some pay out thresholds
-	 * 
 	 */
 	public static void refine(Object somePayoutResults) {
-		
+
 		System.out.println("refine");
-		
+
 		try {
-		Thread.sleep(5000);
-		} catch(Exception  e) {}
-		
-		//At the moment we don't do anything with the
-		//payoff thresholds
-		
-		Object program = null;  //At present the program does nothing
-		
-		//Examine the pay out results
-		//Figure out if we need to alter the program!
-		//For now do nothing
-		
-		//send the program back to replicate
+			Thread.sleep(5000);
+		} catch (Exception e) {
+		}
+
+		// At the moment we don't do anything with the
+		// payoff thresholds
+
+		Object program = null; // At present the program does nothing
+
+		// Examine the pay out results
+		// Figure out if we need to alter the program!
+		// For now do nothing
+
+		// send the program back to replicate
 		replicate(program);
-		
+
 	}
-	
+
 	public static void replicate(Object program) {
-		
+
 		System.out.println("replicate");
-		
+
 		try {
-		Thread.sleep(5000);
-		} catch(Exception  e) {}
-		
-		
-		Object determineCost=null;
-		Object determinePayout=null;
-		
-		Object PayoutResults=null; //Calculate the pay out results based
-							//on the cost and the returns from the program
-		
-		//Run the program here!!!
-		//We imagine we run the program
-		//Right now we do nothing
-		
+			Thread.sleep(5000);
+		} catch (Exception e) {
+		}
+
+		Object determineCost = null;
+		Object determinePayout = null;
+
+		Object PayoutResults = null; // Calculate the pay out results based
+		// on the cost and the returns from the program
+
+		// Run the program here!!!
+		// We imagine we run the program
+		// Right now we do nothing
+
 		refine(PayoutResults);
-		
+
 	}
-	
 
 	public static void main(String[] args) {
-		
+
 		// Theoretically the game runs infinitely
 		// if it can survive
-		
-		Object theMostSimpleProgramEverBeginningLife=null;
 
-		RefineCallsReplicateInfiniteGame.
-			replicate(theMostSimpleProgramEverBeginningLife);
-		
+		Object theMostSimpleProgramEverBeginningLife = null;
+
+		RefineCallsReplicateInfiniteGame
+				.replicate(theMostSimpleProgramEverBeginningLife);
 
 	}
 
