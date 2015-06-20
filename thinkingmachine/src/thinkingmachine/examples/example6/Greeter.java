118188f29ad03d0041788b30e6e26bc2bb5d2bb6
Martin <Martin@WIN-G6RNOCGATRM>
2015-06-20 15:19:03 +0100

    Add some akka stuff - hello world

---
+++ thinkingmachine/src/thinkingmachine/examples/example6/Greeter.java
@@ -0,0 +1,23 @@
+package thinkingmachine.examples.example6;
+
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.actor.ActorRef;
+
+public class Greeter extends UntypedActor {
+    
+    public static enum Msg {
+      GREET, DONE;
+    }
+    
+    @Override
+    public void onReceive(Object msg) {
+      if (msg == Msg.GREET) {
+        System.out.println("Hello World!");
+        getSender().tell(Msg.DONE, getSelf());
+      } else unhandled(msg);
+    }
+    
+  }
+
+
